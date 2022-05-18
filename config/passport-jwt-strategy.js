e9766218f105b28c4780039891ab9aae188b7762
TANISH MOHANTA <tanishmohanta1901@gmail.com>
2022-05-18 17:51:43 +0530

    created password

---
+++ config/passport-jwt-strategy.js
@@ -0,0 +1,39 @@
+const passport=require('passport');
+const { ExtractJwt } = require('passport-jwt/lib');
+const JWTstrategy=require('passport-jwt').Strategy;
+const extractJWT=require('passport-jwt').ExtractJwt;
+
+const User=require('../models/user');
+//while defining jwt-strategy we need to have some option
+//1.encrypt
+//a key is to be present to encrypt any text and decrypt that.
+
+
+let opts={
+    jwtFromRequest:extractJWT.fromAuthHeaderAsBearerToken,
+    secretOrKey:"thisIsKey",
+
+
+}
+
+passport.use(new JWTstrategy(opts,function(jwtPayload,done){
+    User.findById(jwtPayload._id,function(err,user){
+        if(err)
+        {
+            console.log('error in finding user from JWT',err);
+            return;
+        }
+        if(user)
+        {
+            return done(null,user);
+        }
+        else{
+            return done(null,false);
+        }
+    })
+}));
+
+
+
+module.exports=passport;
+//jsonwebtoken regenarates the token.....
---
+++ controllers/api/v1/users_api.js
@@ -0,0 +1,30 @@
+const User=require('../../../models/user');
+const jwt=require('jsonwebtoken');
+
+module.exports.createSession=async function(req,res)
+{
+    //whenever user name and password is received, we need to find that user and generates associated web token corresponding to it.
+    try{
+    let user=User.findOne({email:req.body.email});
+    if(!user||user.password!=req.body.password)
+    {
+        return res.json(422,{
+            message:"invalid username or password"
+        });
+    }
+    return res.json(200,{
+        message:"sign in successfull, here is your token please keep it safe",
+        data:{
+            token:jwt.sign(user.toJSON(),'thisIsKey',{expiresIn:'10000'})
+        }
+    })
+    }
+    catch(err)
+    {
+        console.log(err,'*******');
+        return res.json(500,{
+            message:"internal server error--"
+        });
+    }
+}
+
