7bce33351e5f9d46e572880caf8ba77921fa992a
TANISH MOHANTA <tanishmohanta1901@gmail.com>
2022-05-20 17:09:24 +0530

    completed google authentication

--- routes/users.js
+++ routes/users.js
@@ -24,7 +24,7 @@ router.get('/sign-out', usersController.destroySession);
 
 
 
-router.gett('/auth/google'.passport.authenticate('google',{scope:['profile','email']}));
+router.get('/auth/google',passport.authenticate('google',{scope:['profile','email']}));
 router.get('/auth/google/callback',passport.authenticate('google',{failureRedirect:'/users/sign-in'}),usersController.createSession);
 
 
